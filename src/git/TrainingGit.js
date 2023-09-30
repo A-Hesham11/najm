cb76824d6d5879e7b6428dcc2fdb5df288d088fa
A-Hesham11 <a.hesham2215@gmail.com>
2023-09-30 12:40:35 +0200

    edit in the Training file by the main branch

--- src/git/TrainingGit.js
+++ src/git/TrainingGit.js
@@ -5,6 +5,11 @@ const TrainingGit = () => {
     <div>
       <h2>Najm</h2>
       <p>this is the najm this is the najm</p>
+
+      <div>
+        <h2>i am in the main branch</h2>
+        <p>in the last version the main branch named master but in the new version named main</p>
+      </div>
     </div>
   )
 }
