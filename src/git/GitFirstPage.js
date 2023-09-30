a3f47270a140b897d9ce7cef37f1e996f68bbe44
A-Hesham11 <a.hesham2215@gmail.com>
2023-09-30 12:52:33 +0200

    create new page called GitFirstPage

---
+++ src/git/GitFirstPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GitFirstPage = () => {
+  return (
+    <div>
+      <h2>branch ==`{'>'}` hesham</h2>
+      <p>Git First Page</p>
+    </div>
+  )
+}
+
+export default GitFirstPage
